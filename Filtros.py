cbfd4b9af8102a5eba3c01003c96721567e2fadd
Ivo Pontes <ivopontes@outlook.com>
2018-09-26 11:27:59 -0300

    Menu adicionado

--- Filtros.py
+++ Filtros.py
@@ -22,9 +22,9 @@ class Filtros():
 		#Converte Imagem Object para Matriz
 		self.matriz = np.asarray(img.convert('L'))
 		#Dimensão M
-		self.m = np.size(self.matriz, 1)
+		self.m = np.size(self.matriz, 0)
 		#Dimensão N
-		self.n = np.size(self.matriz, 0)
+		self.n = np.size(self.matriz, 1)
 		print("Linhas: {}\nColunas: {}\n".format(self.m, self.n))
 		print(self.matriz)
 
@@ -32,33 +32,49 @@ class Filtros():
 	'''
 	Interpolação Bilinear para Redução
 	'''
-	def filtroLaplaciano(self):
+	def filtroLaplaciano(self, op):
 		saida = np.zeros([self.m,self.n], dtype=np.uint8)
-		m1 = np.size(saida, 1)
-		n1 = np.size(saida, 0)
+		m1 = np.size(saida, 0)
+		n1 = np.size(saida, 1)
 		print("Linhas: {}\nColunas: {}\n".format(m1,n1))
 
 		#Ternário em Python
 		tamM = self.m-1 if self.m != 0 else self.m
 		tamN = self.n-1 if self.n != 0 else self.n
 
+		#Laplaciano
 
-		mascara = [[-1, -1, -1],
-				  [-1, 8, -1],
-				  [-1, -1, -1]]
-
-		mascara = [[1, 1, 1],
-				  [1, -8, 1],
-				  [1, 1, 1]]
-
-		mascara = [[0, -1, 0],
-				  [-1, 4, -1],
-				  [0, -1, 0]]
-		
-		mascara = [[0, 1, 0],
-				  [1, -4, 1],
-				  [0, 1, 0]]
-		
+		if op == 1:
+			#'''
+			mascara = [[-1, -1, -1],
+					  [-1, 8, -1],
+					  [-1, -1, -1]]
+			#'''
+		elif op == 2:
+			#'''
+			mascara = [[1, 1, 1],
+					  [1, -8, 1],
+					  [1, 1, 1]]
+			#'''
+		elif op == 3:	
+			#'''
+			mascara = [[0, -1, 0],
+					  [-1, 4, -1],
+					  [0, -1, 0]]
+			#'''
+		elif op == 4:		
+			#'''
+			mascara = [[0, 1, 0],
+					  [1, -4, 1],
+					  [0, 1, 0]]
+			#'''
+		elif op == 5:	
+			#Média
+			#'''
+			mascara = [[1, 1, 1],
+					  [1, 1, 1],
+					  [1, 1, 1]]		
+			#'''
 
 
 		#Tratando 
@@ -66,13 +82,14 @@ class Filtros():
 			for j in range(0,tamN):
 				if i != 0 and j != 0 and i != (tamM-1) and j != (tamN-1):
 					#print(self.matriz[i][j], end='\t')
-					saida[i][j] = self.matriz[i-1][j-1]*mascara[0][0] + self.matriz[i-1][j]*mascara[0][1] + self.matriz[i-1][j+1]*mascara[0][2]
-					saida[i][j] += self.matriz[i][j-1]*mascara[1][0] + self.matriz[i][j]*mascara[1][1] + self.matriz[i][j+1]*mascara[1][2]
-					saida[i][j] += self.matriz[i+1][j-1]*mascara[2][0] + self.matriz[i+1][j]*mascara[2][1] + self.matriz[i+1][j+1]*mascara[2][2]
-					saida[i][j] = int(saida[i][j]/9)
+					saida[i][j] = int((self.matriz[i-1][j-1]*mascara[0][0] + self.matriz[i-1][j]*mascara[0][1] + self.matriz[i-1][j+1]*mascara[0][2] + \
+					self.matriz[i][j-1]*mascara[1][0] + self.matriz[i][j]*mascara[1][1] + self.matriz[i][j+1]*mascara[1][2] + \
+					self.matriz[i+1][j-1]*mascara[2][0] + self.matriz[i+1][j]*mascara[2][1] + self.matriz[i+1][j+1]*mascara[2][2])/9)
+
 					if saida[i][j] < 0:
 						saida[i][j] = 0
 
+					#saida[i][j] = 255 - saida[i][j]
 
 
 		for i in range(tamM):
@@ -87,9 +104,9 @@ class Filtros():
 		imagem.show()
 
 	'''
-	Interpolação Bilinear para Redução
+	Aplicação do Filtro de Sobel
 	'''
-	def filtroSobel(self):
+	def filtroSobel(self, op):
 		saida = np.zeros([self.m,self.n], dtype=np.uint8)
 		m1 = np.size(saida, 1)
 		n1 = np.size(saida, 0)
@@ -99,28 +116,34 @@ class Filtros():
 		tamM = self.m-1 if self.m != 0 else self.m
 		tamN = self.n-1 if self.n != 0 else self.n
 
-
-		mascara = [[-1, -2, -1],
-				  [0, 0, 0],
-				  [1, 2, 1]]
-
-		mascara = [[-1, 0, 1],
-				  [-2, 0, 2],
-				  [-1, 0, 1]]
+		#Máscara #1
+		if op == 6:
+			#'''
+			mascara = [[-1, -2, -1],
+					  [0, 0, 0],
+					  [1, 2, 1]]
+			#'''
+		elif op == 7:
+			#Máscara #2
+			#'''
+			mascara = [[-1, 0, 1],
+					  [-2, 0, 2],
+					  [-1, 0, 1]]
+			#'''
 
 		#Tratando 
 		for i in range(0,tamM):
 			for j in range(0,tamN):
 				if i != 0 and j != 0 and i != (tamM-1) and j != (tamN-1):
 					#print(self.matriz[i][j], end='\t')
-					saida[i][j] = self.matriz[i-1][j-1]*mascara[0][0] + self.matriz[i-1][j]*mascara[0][1] + self.matriz[i-1][j+1]*mascara[0][2]
-					saida[i][j] += self.matriz[i][j-1]*mascara[1][0] + self.matriz[i][j]*mascara[1][1] + self.matriz[i][j+1]*mascara[1][2]
-					saida[i][j] += self.matriz[i+1][j-1]*mascara[2][0] + self.matriz[i+1][j]*mascara[2][1] + self.matriz[i+1][j+1]*mascara[2][2]
-					saida[i][j] = int(saida[i][j]/9)
+					saida[i][j] = int((self.matriz[i-1][j-1]*mascara[0][0] + self.matriz[i-1][j]*mascara[0][1] + self.matriz[i-1][j+1]*mascara[0][2] + \
+					self.matriz[i][j-1]*mascara[1][0] + self.matriz[i][j]*mascara[1][1] + self.matriz[i][j+1]*mascara[1][2] + \
+					self.matriz[i+1][j-1]*mascara[2][0] + self.matriz[i+1][j]*mascara[2][1] + self.matriz[i+1][j+1]*mascara[2][2])/9)
 
 					if saida[i][j] < 0:
 						saida[i][j] = 0
 
+					saida[i][j] = 255 - saida[i][j]
 
 		for i in range(tamM):
 			for j in range(tamN):
--- Main.py
+++ Main.py
@@ -6,11 +6,25 @@ from Filtros import Filtros
 if __name__ == '__main__':
 	print("Processamento de Imagens.")
 
-	#filtros = Filtros("img/lena-gray.png")
-	filtros = Filtros("img/sobel-fruits.png")
-	filtros.carregarImagem()
+	op = -1
 
-	#filtros.filtroLaplaciano()
-	filtros.filtroSobel()
+	while op != 0:
+		menuStr = "\n-------Menu-------\nFiltro Laplaciano:\n1 - Tipo #1\n2 - Tipo #2\n3 - Tipo #3\n4 - Tipo #4"
+		menuStr += "\n5 - Filtro de Média:\nFiltro de Sobel:\n6 - Tipo Linhas\n7 - Tipo Colunas\n0 - Sair\n"
+
+		op = input(menuStr)
+		op = int(op)
+
+		if op == 0:
+			pass
+		elif op < 6:
+			filtros = Filtros("img/p.png")
+			filtros.carregarImagem()
+			filtros.filtroLaplaciano(op)
+		else:
+			filtros = Filtros("img/sobel-fruits.png")
+			filtros.carregarImagem()
+			filtros.filtroSobel(op)
+
 			
 	print("Fim execução!!")
